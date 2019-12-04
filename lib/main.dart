2825e34861f32529fcade1a99565f3b2aecb58f5
telami <907848106@qq.com>
2019-12-04 22:25:42 +0800

    [feat] home page refresh

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class TripApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter trip',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,9 +8,11 @@ import 'package:flutter_trip/model/grid_nav_model.dart';
 import 'package:flutter_trip/model/home_model.dart';
 import 'package:flutter_trip/model/sales_box_model.dart';
 import 'package:flutter_trip/widgets/grid_nav.dart';
+import 'package:flutter_trip/widgets/loading_container.dart';
 import 'package:flutter_trip/widgets/local_nav.dart';
 import 'package:flutter_trip/widgets/sales_box.dart';
 import 'package:flutter_trip/widgets/sub_nav.dart';
+import 'package:flutter_trip/widgets/web_view.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 
@@ -20,15 +22,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List _imageUrls = [
-    "https://dimg03.c-ctrip.com/images/10020q000000gajaeDB6E_C_750_420_Q90.jpg",
-    "https://dimg03.c-ctrip.com/images/200f0v000000k0r3361EC_C_750_420_Q90.jpg",
-    "https://dimg03.c-ctrip.com/images/200j0t000000ih4ttD0C5_C_750_420_Q90.jpg",
-  ];
-
   double appBarAlpha = 0;
 
   List<CommonModel> localNavList;
+
+  List<CommonModel> bannerList = [];
 
   GridNavModel gridNav;
 
@@ -36,94 +34,122 @@ class _HomePageState extends State<HomePage> {
 
   SalesBoxModel salesBox;
 
+  bool _loading = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0xfff2f2f2),
-        body: Stack(
-          children: <Widget>[
-            MediaQuery.removePadding(
-                removeTop: true,
-                context: context,
-                child: NotificationListener(
-                  // ignore: missing_return
-                  onNotification: (scrollNotification) {
-                    if (scrollNotification is ScrollUpdateNotification &&
-                        scrollNotification.depth == 0) {
-                      _onScroll(scrollNotification.metrics.pixels);
-                    }
-                  },
-                  child: ListView(
-                    children: <Widget>[
-                      Container(
-                        height: 160,
-                        child: Swiper(
-                          itemCount: _imageUrls.length,
-                          autoplay: true,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Image.network(_imageUrls[index],
-                                fit: BoxFit.fill);
-                          },
-                          pagination: SwiperPagination(),
+        body: LoadingContainer(
+            isLoading: _loading,
+            child: Stack(
+              children: <Widget>[
+                MediaQuery.removePadding(
+                    removeTop: true,
+                    context: context,
+                    child: RefreshIndicator(
+                      onRefresh: _handleRefresh,
+                      child: NotificationListener(
+                        // ignore: missing_return
+                        onNotification: (scrollNotification) {
+                          if (scrollNotification is ScrollUpdateNotification &&
+                              scrollNotification.depth == 0) {
+                            _onScroll(scrollNotification.metrics.pixels);
+                          }
+                        },
+                        child: ListView(
+                          children: <Widget>[
+                            Container(
+                              height: 160,
+                              child: Swiper(
+                                itemCount: bannerList.length,
+                                autoplay: true,
+                                itemBuilder: (BuildContext context, int index) {
+                                  var banner = bannerList[index];
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context).push(MaterialPageRoute(
+                                          builder: (context) => WebView(
+                                            url: banner.url,
+                                            statusBarColor: banner.statusBarColor,
+                                            hideAppBar: banner.hideAppBar,
+                                          )));
+                                    },
+                                    child: Image.network(banner.icon,
+                                        fit: BoxFit.fill),
+                                  );
+                                },
+                                pagination: SwiperPagination(),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                              child: LocalNav(
+                                list: localNavList,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                              child: GridNavWidget(
+                                gridNav: gridNav,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                              child: SubNav(
+                                list: subNavList,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                              child: SalesBox(
+                                salesBox: salesBox,
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-                        child: LocalNav(
-                          list: localNavList,
+                    )),
+                Opacity(
+                    opacity: appBarAlpha,
+                    child: Container(
+                      height: 80,
+                      decoration: BoxDecoration(color: Colors.white),
+                      child: Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: Text('首页'),
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-                        child: GridNavWidget(
-                          gridNav: gridNav,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-                        child: SubNav(
-                          list: subNavList,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-                        child: SalesBox(
-                          salesBox: salesBox,
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-            Opacity(
-                opacity: appBarAlpha,
-                child: Container(
-                  height: 80,
-                  decoration: BoxDecoration(color: Colors.white),
-                  child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 20),
-                      child: Text('首页'),
-                    ),
-                  ),
-                )),
-          ],
-        ));
+                    )),
+              ],
+            )));
   }
 
   @override
   void initState() {
     super.initState();
-    _loadData();
+    _handleRefresh();
   }
 
-  _loadData() async {
-    HomeModel home = await HomeApi.fetch();
-    setState(() {
-      localNavList = home.localNavList;
-      gridNav = home.gridNav;
-      subNavList = home.subNavList;
-      salesBox = home.salesBox;
-    });
+  Future<Null> _handleRefresh() async {
+    try {
+      HomeModel home = await HomeApi.fetch();
+      setState(() {
+        localNavList = home.localNavList;
+        bannerList = home.bannerList;
+        gridNav = home.gridNav;
+        subNavList = home.subNavList;
+        salesBox = home.salesBox;
+        _loading = false;
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        _loading = false;
+      });
+    }
+    return null;
   }
 
   _onScroll(double offset) {
